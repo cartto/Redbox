5a5e9e3d108330c0daac29d516ad114b06618d41
catto <75825187+cartto@users.noreply.github.com>
2022-07-15 16:51:29 -0700

    added chatevent

--- vars.lua
+++ vars.lua
@@ -3,11 +3,12 @@ vars.lplr = game.Players.LocalPlayer
 vars.plr = game.Players
 vars.Character = vars.lplr.Character
 vars.Mouse =  vars.lplr:GetMouse().KeyDown
+vars.ChatEvent = vars.lplr.Chatted
 
 function vars.getPlrs()
     for k, v in next, vars.plr:GetPlayers() do
         return v
     end
 end
---i hope
+
 return vars
