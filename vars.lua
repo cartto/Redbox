66b6dd45d18ab7cdca6403ebc249aea3820aaefc
catto <75825187+cartto@users.noreply.github.com>
2022-07-15 16:19:28 -0700

    Update vars.lua

--- vars.lua
+++ vars.lua
@@ -1,12 +1,13 @@
-local game = {  }
-game.lplr = game.Players.LocalPlayer
-game.plr = game.Players
-game.Character = lplr.Character
-game.Mouse =  lplr:GetMouse().KeyDown
+local vars = {  }
+vars.lplr = game.Players.LocalPlayer
+vars.plr = game.Players
+vars.Character = vars.lplr.Character
+vars.Mouse =  vars.lplr:GetMouse().KeyDown
 
-function game.getPlrs()
-    for k, v in next, game.plr:GetPlayers() do
+function vars.getPlrs()
+    for k, v in next, vars.plr:GetPlayers() do
         return v
     end
 end
-return game
+--i hope
+print(vars.lplr.Name)
