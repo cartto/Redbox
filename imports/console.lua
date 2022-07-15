9f7a5934c46580a46d1c586bb19cb2ce726a325c
catto <75825187+cartto@users.noreply.github.com>
2022-07-15 16:58:40 -0700

    Add files via upload

---
+++ imports/console.lua
@@ -0,0 +1,7 @@
+local console = {   }
+
+console.print = rconsoleprint()
+console.info = rconsoleinfo()
+console.warn = rconsolewarn()
+console.clr = rconsoleclear()
+console.input = rconsoleinput()
