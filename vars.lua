1f0110becca21e3acb3fdb0a4c9c5cdb68024199
catto <75825187+cartto@users.noreply.github.com>
2022-07-15 15:59:01 -0700

    Update vars.lua
    added vars

--- vars.lua
+++ vars.lua
@@ -1,1 +1,10 @@
+local lplr = game.Players.LocalPlayer
+local plr = game.Players
+local Character = lplr.Character
+local Mouse =  lplr:GetMouse().KeyDown
 
+function getPlrs()
+    for k, v in next, plr:GetPlayers() do
+        return v
+    end
+end
