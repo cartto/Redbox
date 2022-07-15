44e143c88e3d52329999b3b504860ab1ab025c53
catto <75825187+cartto@users.noreply.github.com>
2022-07-15 16:17:05 -0700

    Add files via upload

---
+++ vars.lua
@@ -0,0 +1,12 @@
+local game = {  }
+game.lplr = game.Players.LocalPlayer
+game.plr = game.Players
+game.Character = lplr.Character
+game.Mouse =  lplr:GetMouse().KeyDown
+
+function game.getPlrs()
+    for k, v in next, game.plr:GetPlayers() do
+        return v
+    end
+end
+return game
