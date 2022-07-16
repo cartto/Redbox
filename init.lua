57577ce2b1af43f0191f1e05bc17a62af63e49fd
catto <75825187+cartto@users.noreply.github.com>
2022-07-15 17:28:12 -0700

    I FIXED IT

--- init.lua
+++ init.lua
@@ -1,9 +1,9 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/cartto/Redbox/main/import.func", true))()
 
-    local console = import("imports/console.lua")
-    local vars = import("imports/vars.lua")
+local console = import("imports/console.lua")
+local vars = import("imports/vars.lua")
 
-    vars.print("@@CYAN@@")
-    vars.print("Welcome "..vars.lplr.Name.." RedBox loaded sucessfully")
-    vars.info("Script made by carto#8954")
-    vars.print("Type help in this prompt to get a list of commands")
+console.print("@@CYAN@@")
+console.print("Welcome "..vars.lplr.Name.." RedBox loaded sucessfully\n")
+console.info("Script made by carto#8954")
+console.info("Type help in this prompt to get a list of commands")
