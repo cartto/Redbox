eae1895f8faae139ce006220d710c512ccb1d29a
catto <75825187+cartto@users.noreply.github.com>
2022-07-15 18:22:25 -0700

    removed unimportant shit

--- main/main.lua
+++ main/main.lua
@@ -1,1 +1,20 @@
-return "YOUR MOTHER LMAO"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/cartto/Redbox/main/import.func", true))()
+
+local init = import("init.lua")
+local vars = import("imports/vars.lua")
+
+local Cmds = {...}
+
+function Cmds.Add(name, callback)
+    Cmds[name:lower()] = callback
+end
+
+
+function Cmds.Run()
+    vars.ChatEvent:Connect(function(msg)
+        getgenv().msgEvent = msg
+        if Cmds[msg] then
+            Cmds[msg](msg)
+        end
+    end)
+end
