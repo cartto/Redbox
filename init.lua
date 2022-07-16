10becc0c7e6affef004e6ca825df3782e1e8d6b3
catto <75825187+cartto@users.noreply.github.com>
2022-07-15 23:21:31 -0700

    removed input

--- init.lua
+++ init.lua
@@ -6,31 +6,4 @@ local vars = import("imports/vars.lua")
 console.print("@@CYAN@@")
 console.print("Welcome "..vars.lplr.Name.." RedBox loaded sucessfully\n")
 console.info("Script made by carto#8954")
-console.info("Type help in this prompt to get a list of commands")
-
-Http = game:GetService("HttpService")
-
-getgenv().loopInput = true
-
-coroutine.wrap(function()
-    repeat wait() until isfile("Commands.redbox")
-        print("making")
-        while getgenv().loopInput do wait()    
-        if console.input() == "help" then
-                Http = game:GetService("HttpService")
-                a=Http:JSONDecode(readfile("Commands.redbox"))
-                for i,v in next,a do
-                print(i..": "..v)
-            end     
-        end
-    end
-end)()
-
-
-coroutine.wrap(function()
-        while true do wait()    
-        if console.input() == "exit" then
-            game:shutdown()
-        end
-    end
-end)()
+console.info("Click P in roblox to get a list of commands")
