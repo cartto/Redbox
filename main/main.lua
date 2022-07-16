df550f874f58dd231e0088365ee3537fcfd3ca3a
catto <75825187+cartto@users.noreply.github.com>
2022-07-15 18:14:14 -0700

    Update main.lua

--- main/main.lua
+++ main/main.lua
@@ -17,12 +17,11 @@ function Cmds.Run(name)
     end
 end
 
-function Cmds.Chat()
-    vars.ChatEvent:Connect(function(msg)
-        if Cmds[msg] then
-            Cmds.Run(msg)
-        end
-    end)
-end
+vars.ChatEvent:Connect(function(msg)
+     getgenv().msgEvent = msg
+    if Cmds[getgenv().msgEvent] then
+        Cmds.Run(getgenv().msgEvent)
+    end
+end)
 
 return Cmds
