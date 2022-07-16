6315cc27a5a94d291e585c767ee3756db35f80ce
catto <75825187+cartto@users.noreply.github.com>
2022-07-15 17:35:32 -0700

    Update main.lua

--- main/main.lua
+++ main/main.lua
@@ -1,1 +1,18 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/cartto/Redbox/main/import.func", true))()
 
+local init = import("init.lua")
+local vars = import("imports/vars.lua")
+
+local Cmds = {...}
+
+function Cmds.Add(name, callback)
+    Cmds[name:lower()] = callback(vars.lplr)
+end
+
+function Cmds.Run(name, callback)
+    if Cmds[name:lower()] then
+        Cmds[name]()
+    else
+        return false
+    end
+end
