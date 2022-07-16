6729b2b158a1cf9cee85cfc02ad65b44a1ee1593
catto <75825187+cartto@users.noreply.github.com>
2022-07-15 17:21:49 -0700

    trying to fix broken stuff

--- init.lua
+++ init.lua
@@ -1,9 +1,11 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/cartto/Redbox/main/import.func", true))()
+function init()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/cartto/Redbox/main/import.func", true))()
 
-local console = import("imports/console.lua")
-local vars = import("imports/vars.lua")
+    local console = import("imports/console.lua")
+    local vars = import("imports/vars.lua")
 
-vars.print("@@CYAN@@")
-vars.print("Welcome "..vars.lplr.Name.." RedBox loaded sucessfully")
-vars.info("Script made by carto#8954")
-vars.print("Type help in this prompt to get a list of commands")
+    vars.print("@@CYAN@@")
+    vars.print("Welcome "..vars.lplr.Name.." RedBox loaded sucessfully")
+    vars.info("Script made by carto#8954")
+    vars.print("Type help in this prompt to get a list of commands")
+end
