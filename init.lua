b0ef260e809d551ca54d405b40418eeed6e8cb82
catto <75825187+cartto@users.noreply.github.com>
2022-07-15 17:11:45 -0700

    added loads

--- init.lua
+++ init.lua
@@ -1,2 +1,9 @@
-local vars = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/cartto/Redbox/main/vars.lua"))()
-vars.Character.Humanoid.WalkSpeed = 100
+loadstring(game:HttpGet("https://raw.githubusercontent.com/cartto/Redbox/main/import.func", true))()
+
+local console = import("imports/console.lua")
+local vars = import("imports/vars.lua")
+
+vars.print("@@CYAN@@")
+vars.print("Welcome "..vars.lplr.Name.." RedBox loaded sucessfully")
+vars.info("Script made by carto#8954")
+vars.print("Type help in this prompt to get a list of commands")
