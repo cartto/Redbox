66c0f8238de7aa7af608ff384f746d88754d919c
catto <75825187+cartto@users.noreply.github.com>
2022-07-15 17:38:38 -0700

    added cmds.chat

--- main/main.lua
+++ main/main.lua
@@ -9,12 +9,20 @@ function Cmds.Add(name, callback)
     Cmds[name:lower()] = callback(vars.lplr)
 end
 
-function Cmds.Run(name, callback)
+function Cmds.Run(name)
     if Cmds[name:lower()] then
         Cmds[name]()
     else
         return false
     end
+end
+
+function Cmds.Chat()
+    vars.ChatEvent:Connect(function(msg)
+        if Cmds[msg] then
+            Cmds.Run(msg)
+        end
+    end)
 end
 
 return Cmds
